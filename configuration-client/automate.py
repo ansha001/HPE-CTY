a1833597f74b298c858bed755feb18f74c02a7d0
Ansha Prashanth <97624380+ansha001@users.noreply.github.com>
2023-06-05 16:57:07 +0530

    Update automate.py

--- configuration-client/automate.py
+++ configuration-client/automate.py
@@ -4,11 +4,14 @@ superUser = True if os.geteuid() == 0 else False
 
 if superUser:
 	os.system('touch /etc/systemd/system/test.service')
-        with open('/etc/systemd/system/test.service','w') as f:
-                f.write('[Unit]\n')
-                f.write('Description=My Boot Script\n')
+	with open('/etc/systemd/system/test.service','w') as f:
+                f.write('[Unit]')
+                f.write('\n')
+                f.write('Description=My Boot Script')
+                f.write('\n\n')
                 f.write('After=network.target\n')
                 f.write('[Service]\n')
-                f.write('ExecStart=/HPE-CTY/configuration-client/bootupfile.py\n')
+                f.write('ExecStart=/usr/bin/python3 /HPE-CTY/configuration-client/bootupfile.py')
+                f.write('\n\n')
                 f.write('[Install]\n')
-                f.write('WantedBy=default.target\n')
+                f.write('WantedBy=default.target\n\n')
